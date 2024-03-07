2b7bafb6f8db4daff5ca49c988e13f342f51aae2
Hezron Bwareng <bwarenghezron@gmail.com>
2024-03-07 09:24:37 +0000

    listing footer and header

--- listing.js
+++ listing.js
@@ -12,27 +12,35 @@ document.addEventListener('DOMContentLoaded', function() {
                 return response.json();
             })
             .then(data => {
-                data.forEach(property => {
-                    const propertyElement = document.createElement('div');
-                    propertyElement.classList.add('property');
+                if (data.length === 0) {
+                    const notFoundMessage = document.createElement('div');
+                    notFoundMessage.classList.add('not-found-message');
+                    notFoundMessage.innerHTML = `<p>No properties found matching your search term. 🙁 </p>`;
+                    propertyListings.appendChild(notFoundMessage);
+                } else {
+                    data.forEach(property => {
+                        const propertyElement = document.createElement('div');
+                        propertyElement.classList.add('property');
 
-                    propertyElement.innerHTML = `
-                        <h2>${property.location}</h2>
-                        <p>Category: ${property.category}</p>
-                        <p>Bedrooms: ${property.bed_rooms}</p>
-                        <p>Price Per Month: ${property.price_per_month}</p>
-                        <p>Date Added: ${property.date_added}</p>
-                        <p>Amenities: ${property.amenities}</p>
-                        <p>Agent: ${property.agent_who_added}</p>
-                    `;
+                        propertyElement.innerHTML = `
+                            <h2>${property.location}</h2>
+                            <p>Category: ${property.category}</p>
+                            <p>Bedrooms: ${property.bed_rooms}</p>
+                            <p>Price Per Month: ${property.price_per_month}</p>
+                            <p>Date Added: ${property.date_added}</p>
+                            <p>Amenities: ${property.amenities}</p>
+                            <p>Agent: ${property.agent_who_added}</p>
+                        `;
 
-                    propertyListings.appendChild(propertyElement);
-                });
+                        propertyListings.appendChild(propertyElement);
+                    });
+                }
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
                 const errorMessage = document.createElement('div');
-                errorMessage.textContent = 'Failed to fetch data. Please try again later.';
+                errorMessage.classList.add('error-message');
+                errorMessage.innerHTML = `<p>Failed to fetch data. Please try again later. 🙁 </p>`;
                 propertyListings.appendChild(errorMessage);
             });
     }
